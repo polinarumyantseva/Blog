68a7dc29404dbdc4199d2258cad76010de39cc59
polinarumyantseva <polinrumyantseva@gmail.com>
2025-02-16 15:31:32 -0800

    Реализовать основной компонент с роутингом

---
+++ src/Blog.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import styles from './app.module.css';
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
+
+export const Blog = () => {
+	return (
+		<div className={styles.app}>
+			<Header />
+			<div className={styles.content}>
+				<h2 className={styles.title}>Контент страницы</h2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/login" element={<div>Авторизация</div>} />
+					<Route path="/register" element={<div>Регистрация</div>} />
+					<Route path="/users" element={<div>Пользователи</div>} />
+					<Route path="/post" element={<div>Новая статья</div>} />
+					<Route path="/post/:postId" element={<div>Статья</div>} />
+					<Route path="*" element={<div>Ошибка</div>} />
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	);
+};
